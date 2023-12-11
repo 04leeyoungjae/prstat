c1f62d7f17ee0c3352ee4dce539f5d20a574de93
karinysis <leeyoungjae@pusan.ac.kr>
2023-12-11 22:50:42 +0900

    add function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f: # README.md 내용 읽어오기
 
 setup(
     name='prstat',
-    version='1.0.1',
+    version='1.0.4',
     license='MIT',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
@@ -16,3 +16,8 @@ setup(
     packages=find_packages(),    
     install_requires=['sympy'],
 )
+
+"""
+python setup.py sdist bdist_wheel
+twine upload dist/*
+"""
